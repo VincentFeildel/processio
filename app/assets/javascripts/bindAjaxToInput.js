b65388232756b68531b755664d12c8951126de6e
Olivier Sesboue <sesboue.olivier@gmail.com>
2017-08-22 18:59:54 +0200

    ajax operational on index

--- app/assets/javascripts/bindAjaxToInput.js
+++ app/assets/javascripts/bindAjaxToInput.js
@@ -1,7 +1,11 @@
-document.addEventListener("change", function() {
-    if(activities.value == "addNew")
-    {
-        addActivityItem();
-    }
-});
+const checkboxes = document.querySelectorAll(".checkbox-input");
+
+if (checkboxes.length > 0) {
+  checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener("change", function() {
+      const form = document.getElementById('filter-form');
+      form.submit();
+    });
+  })
+}
 
