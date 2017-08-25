c91a1955c187045c833fbacf9fbcaffa7eec4b65
Vincent Feildel <vincent.feildel@gmail.com>
2017-08-25 14:53:17 +0200

    modal mail pdf

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   resources :events, only: [:index, :show] do
-    get 'owner_contacted', to: "events#owner_contacted"
-    get 'tenant_notified', to: "events#tenant_notified"
     get 'letter', to: 'events#letter'
+    get 'mail', to: 'events#mail'
     resources :comments
   end
   resources :leases, only: [:new, :index]
