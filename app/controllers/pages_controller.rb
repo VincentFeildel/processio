8499f2075d7cb4f400e9782aa7e1b68a9eaea2bb
Vincent Feildel <vincent.feildel@gmail.com>
2017-08-26 19:43:30 +0200

    rmv public assets and pimping home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,18 +3,19 @@ class PagesController < ApplicationController
     @user = current_user
 
     # Circular progress bar data:
-    @percentage_done = (Event.where(to_do: false).count.fdiv(Event.all.count)*100).round
+    @percentage_finished = (Event.where(to_do: false).count.fdiv(Event.all.count)*100).round
     p = 0
     Event.where(status: 'tenant_to_notify').each do |e|
       p += 1 if e.description != 'retard loyer'
     end
     @percentage_progress = (p.fdiv(Event.all.count)*100).round
-
+    @percentage_done = (@percentage_progress/2) + @percentage_finished
     #Cards data
     @nbr_urgent = Event.where(emergency_level: 'urgent').count
     @nbr_rent_rev = Event.where(description: 'révision de loyer').count
     @nbr_late_rent = Event.where(description: 'retard loyer').count
     @nbr_end_lease = Event.where(description: 'fin de bail').count
+    @nbr_total = Event.count
 
     # Graph data:
     @labels = []
