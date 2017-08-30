ffb84d4a294a967f2f4f4eb458ff16ca5d489412
Olivier Sesboue <sesboue.olivier@gmail.com>
2017-08-30 11:42:23 +0200

    mailer up and runing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  # config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :postmark
+  config.action_mailer.postmark_settings   = { api_key: ENV['POSTMARK_API_KEY'] }
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
